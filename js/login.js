c089507dbdd1d380a2132e5ba790e75600f96b9a
Juan <juanbocca321@gmail.com>
2020-08-04 20:28:37 -0300

    google sign in v1

--- js/login.js
+++ js/login.js
@@ -1,6 +1,16 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+onSignIn();
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+
+    if (!null(profile.getId())) {
+        location.href = 'index.html';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
 
 });
